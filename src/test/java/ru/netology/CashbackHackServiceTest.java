61a619d18c8f55ee79bc3b237e85fbe763c7a57e
Neto test <o.sirotina@inbox.ru>
2023-06-28 20:03:28 +0300

    изменение тестов

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -25,7 +25,7 @@ public class CashbackHackServiceTest {
         CashbackHackService service = new CashbackHackService();
         int amount = 1000;
         int actual = service.remain(amount);
-        int expected = 1000;
+        int expected = 0;
         assertEquals(expected,actual);
     }
 
